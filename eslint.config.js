572dda7754c42e8b76847337c699ee3e9ec37dcd
Kateryna Pereverzeva <katya.pereverzeva2109@gmail.com>
2025-06-25 15:47:29 +0200

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,17 @@
 export default [
-  {
-    files: ['**/*.js'], // Lint JS files
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+    {
+      files: ['**/*.ts'], // Lint ts files
+      languageOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      rules: {
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+        indent: ['error', 2],
+        'no-unused-vars': 'warn',
+        'no-console': 'off',
+        'eqeqeq': ['error', 'always'],
+      },
     },
-    rules: {
-      semi: ['error', 'always'],
-      quotes: ['error', 'single'],
-      indent: ['error', 2],
-      'no-unused-vars': 'warn',
-      'no-console': 'off',
-      'eqeqeq': ['error', 'always'],
-    },
-  },
-];
+  ];
